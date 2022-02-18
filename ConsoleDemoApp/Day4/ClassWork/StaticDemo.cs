b23dbb26517b4275a76a259e946afe00af6db23a
hridayverma <hrid.06@gmail.com>
2022-02-18 14:18:39 +0530

    Add project files.

---
+++ ConsoleDemoApp/Day4/ClassWork/StaticDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemoApp.Day4.ClassWork
+{
+    internal static class StaticDemo
+    {
+        public static int id;
+        static StaticDemo()
+        {
+            Console.WriteLine("Static constr");
+            id = 1000;
+        }
+        public static void Display() {
+            Console.WriteLine($"{id}");
+        }
+    }
+    class StaticMain
+    {       
+        static void Main(string[] args)
+        {
+            //StaticDemo obj;//= new StaticDemo();
+            //obj.Dispaly()
+            StaticDemo.Display();
+            Console.WriteLine(StaticDemo.id);
+        }
+    }
+}
---
+++ ConsoleDemoApp/Day4/ClassWork/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemoApp.Day4.ClassWork
+{
+    /*
+       1-methods
+       2-constructor-fails
+       3-By making data fields public,incorrect way
+    */
+    internal class Student
+    {
+        int _RollNo;
+        string _Name;
+        int _Age;
+        public void SetRoll(int roll) {
+            this._RollNo = roll;//write operation
+        }
+        public int GetRoll()
+        {
+            return this._RollNo;//read operation
+        }
+
+    }
+
+    class StudentMain {
+        static void Main(string[] args)
+        {
+            Student obj = new Student();
+           // obj._RollNo = 2000;
+           // Console.WriteLine(obj._RollNo);
+        }
+    }
+}

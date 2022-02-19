691851c75cac764e034724a30d23219da159cc70
hridayverma <hrid.06@gmail.com>
2022-02-19 16:39:17 +0530

    Arrays work

--- ConsoleDemoApp/Day6/ArrayDemo1.cs
+++ ConsoleDemoApp/Day6/ArrayDemo1.cs
@@ -8,5 +8,72 @@ namespace ConsoleDemoApp.Day6
 {
     internal class ArrayDemo1
     {
+        static void Main(string[] args)
+        {
+            //Normal Array
+            // int[] arr;//decln
+            // arr = new int[5];//intialization
+            //int[, ,] arr = new int[5,4,5];//decln + intialization with default values
+            //Console.WriteLine(arr.Length);//no of elements stored in array
+            //Console.WriteLine(arr.Rank);//dimension of array
+
+            //Console.WriteLine(arr.GetUpperBound(0));//4
+            //Console.WriteLine(arr.GetUpperBound(1));//3
+            //Console.WriteLine(arr.GetUpperBound(2));//4
+            //Console.WriteLine(arr.GetLength(0));//5
+            //Console.WriteLine(arr.GetLength(1));//4
+            //Console.WriteLine(arr.GetLength(2));//5
+            // int[] arr = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            int[] arr ={ 10, 2, 3, 40, 5, 60 };
+
+            Console.WriteLine("Arrays elements are:");
+            //for (int i = 0; i <arr.Length ; i++)
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            for (int i = 0; i <=arr.GetUpperBound(0); i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            //Console.WriteLine("Input array values:");
+            //for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            //{
+            //    Console.WriteLine($"Enter element {i+1} value:");
+            //    arr[i]=Int32.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Current Arrays elements are:");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Console.WriteLine(arr.Min());
+            Console.WriteLine(arr.Max());
+            Console.WriteLine(arr.Average());
+
+            Array.Sort(arr);
+           // Array.Reverse(arr);
+            Console.WriteLine("Sorted Arrays elements are:");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }            
+            int x = 60;
+            bool res = arr.Contains(x);
+            Console.WriteLine("result:"+res);
+
+            //int[] arr2 = { 10, 20, 30, 40, 50, 60 };
+            int[] arr2 = arr;//copy
+            int result= Array.BinarySearch<int>(arr2, x);
+             Console.WriteLine("result:" + result);
+
+
+
+        }
     }
 }
+
+
+
+
---
+++ ConsoleDemoApp/Day6/ArrayDemo2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemoApp.Day6
+{
+    internal class ArrayDemo2
+    {
+        public static void Display(int[][]arr) {
+
+            Console.WriteLine("matrix values are:");
+            for (int r = 0; r < arr.Length; r++)
+            {
+                for (int c = 0; c < arr[r].Length; c++)
+                {
+                    Console.Write($"{arr[r][c]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            //int[][] jagged = new int[4][];//4 1-D array 
+
+            //jagged[0] = new int[5] { 1,2,3,4,5};
+            //jagged[1] = new int[3] { 6,7,8};
+            //jagged[2] = new int[2] { 9,10};
+            //jagged[3] = new int[4] { 11,12,13,14};
+
+            int[][] jagged = new int[4][]
+                            {
+                                new int[5]{ 1,2,3,4,5},//5,1
+                                new int[3] { 6,7,8},   //3,1
+                                new int[2] { 9, 10 },  //2,1
+                                new int[4] { 11, 12, 13, 14 }//4,1
+                            };          
+
+            Console.WriteLine("Rank :"+jagged.Rank);//1
+            Console.WriteLine("Length :" + jagged.Length);//4
+            Console.WriteLine(jagged[0].Length);
+            Console.WriteLine(jagged[0].Rank);
+            Display(jagged);
+
+
+        }
+    }
+}
